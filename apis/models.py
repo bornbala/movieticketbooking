3db5ddce96ea0f6eacb158ca1b7019ca346c546f
bornbala <77392586+bornbala@users.noreply.github.com>
2025-03-13 09:38:02 +0530

    Update models.py

--- apis/models.py
+++ apis/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     password = models.CharField(max_length=255)
