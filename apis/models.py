a29df69ce69c2ffe0a27837e95244a382fd82dcc
bornbala <77392586+bornbala@users.noreply.github.com>
2025-03-13 09:36:07 +0530

    Update models.py

--- apis/models.py
+++ apis/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.core.validators import validate_email
 
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(unique=True,validators=[validate_email])
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
